1e8ed98cd551ee2ebd1813e1dba9899393ff859b
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2022-12-08 22:23:09 +0530

    updated

--- src/context/types.js
+++ src/context/types.js
@@ -6,21 +6,24 @@ export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
 export const AUTH_ERROR = "AUTH_ERROR";
 
+// Generic
+export const SET_TRIGGER = "SET_TRIGGER";
+export const CLEAR_TRIGGER = "CLEAR_TRIGGER";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 export const SET_EMAIL_VERIFIED = "SET_EMAIL_VERIFIED";
-
 export const CLEAR_EMAIL_VERIFIED = "CLEAR_EMAIL_VERIFIED";
 export const CLEAR_IS_REGISTERED = "CLEAR_IS_REGISTERED";
-
 export const FORGOT_PASSWORD = "FORGOT_PASSWORD";
 export const RESET_PASSWORD = "RESET_PASSWORD";
 
-export const SET_TRIGGER = "SET_TRIGGER";
-export const CLEAR_TRIGGER = "CLEAR_TRIGGER";
-
-
+// User
 export const USER_LOADED = "USER_LOADED";
 
-export const CLEAR_ERRORS = "CLEAR_ERRORS";
+//Posts
+export const SET_POSTS = "SET_POSTS"
+export const ADD_POST = "ADD_POST"
+export const POSTS_ERROR = "POSTS_ERROR"
+
 
 // Current
 export const SET_CURRENT = "SET_CURRENT";
